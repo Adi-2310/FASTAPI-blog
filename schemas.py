7965842ba1f278722f331f2d73250439eefb22cc
Aditya Nair <adi@Adityas-MacBook-Air-2.local>
2026-06-16 10:20:26 +0530

    Added Pagination along with multiple profile images

--- schemas.py
+++ schemas.py
@@ -1,42 +1,53 @@
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
 
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
 
+
 class UserCreate(UserBase):
     password: str = Field(min_length=8)
 
-class UserPublic(UserBase):
+
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
     username: str
     image_file: str | None
-    image_path: str  
+    image_path: str
+
 
 class UserPrivate(UserPublic):
     email: EmailStr
+
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
 
+
 class PostCreate(PostBase):
     pass
 
+
 class PostUpdate(BaseModel):
-    title: str | None = Field(default=None,min_length=1, max_length=100)
+    title: str | None = Field(default=None, min_length=1, max_length=100)
     content: str | None = Field(default=None, min_length=1)
+
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
@@ -45,3 +56,11 @@ class PostResponse(PostBase):
     user_id: int
     date_posted: datetime
     author: UserPublic
+
+
+class PaginatedPostsResponse(BaseModel):
+    posts: list[PostResponse]
+    total: int
+    skip: int
+    limit: int
+    has_more: bool
